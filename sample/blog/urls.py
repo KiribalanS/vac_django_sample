818c9a30d698b254c41a55b95fbe6dee97d20901
knight _ wolf <kiribalanshanmugam@gmail.com>
2023-08-18 21:38:03 +0530

    end of the day

--- sample/blog/urls.py
+++ sample/blog/urls.py
@@ -2,5 +2,13 @@ from  django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home )
+    path('', views.home ),
+    path('s/', views.shop),
+    path('college/', views.college),
+    path('html/', views.html),
+    path('text', views.text),
+    path('form', views.form),
+    path('submit', views.myf),
+    path('para/<str:name>', views.para ),
+
 ]
--- sample/blog/views.py
+++ sample/blog/views.py
@@ -5,3 +5,33 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Lunch Time")
+
+def shop(request):
+    return HttpResponse("Shop VCET")
+
+def college(request):
+    return HttpResponse("Velalar college of engineering and technology")
+
+def html(request):
+    return render(request, 'index.html')
+def form(request):
+    return render(request, 'form.html')
+
+def text(request):
+    return render(request, 'text.html',
+                   context= {
+        "text": "the context text"
+    })
+
+def para(request, name):
+    return render(
+        request, 'url.html',
+        context={
+            "url": name
+        }
+    )
+
+def myf(request):
+    name = request.POST.get("name")
+    print(name)
+    return HttpResponse("Form Submitted!")
